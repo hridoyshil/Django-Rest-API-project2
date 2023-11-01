5a8c8bc92c9be601a5116c11ab17ae597fba9b4e
Hridoy <hriodykumarshil12@gmail.com>
2023-11-01 23:46:00 +0600

    Create Seassion Authentication in Django REST Framework
    Seassion Authentication--  Django's default session backend for authentication.
    
    https://www.django-rest-framework.org/api-guide/authentication/#sessionauthentication
    
    https://www.youtube.com/watch?v=PSCcwcAwhv4&list=PLKdU0fuY4OFfqVYY6LucrKRvvbVJME7Vf&index=29

--- djangorest/djangorest/settings.py
+++ djangorest/djangorest/settings.py
@@ -126,8 +126,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 REST_FRAMEWORK = {
+    # "DEFAULT_AUTHENTICATION_CLASSES": [
+    #     "rest_framework.authentication.BasicAuthentication",
+    # ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
