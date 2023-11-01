2869966f6d957c9caa77f8576b03c5e0e83364df
Hridoy <hriodykumarshil12@gmail.com>
2023-11-01 23:31:00 +0600

    create Basic Authentication in Django REST
    https://www.django-rest-framework.org/api-guide/authentication/#basicauthentication
    
    https://www.youtube.com/watch?v=ZL9xpWkLeAI&list=PLKdU0fuY4OFfqVYY6LucrKRvvbVJME7Vf&index=29

--- djangorest/drapi/views.py
+++ djangorest/drapi/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from .serializers import Aiquestserializer
 from .models import Aiquest
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 
 class Aiquest_Model_ViewSet(viewsets.ModelViewSet):
     # List, Create, delete, update, partinal, patch
     queryset = Aiquest.objects.all()
     serializer_class = Aiquestserializer
+    permission_classes = [IsAdminUser]
